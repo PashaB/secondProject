dc6a6b809690850cce584d235cfbe041da469a0a
Paul Boguslavskiy <boguslavskiy@gmail.com>
2017-01-23 00:15:14 -0500

    Manipulating strings to create a usable object I can send to d3 in the future.

--- server.js
+++ server.js
@@ -38,7 +38,39 @@ app.listen(3000, function() {
 app.get('/', (req,res) => {
 		
 		var logmystuff = function(input) {
-			console.dir(input);
+			
+			//Function to modify the string, this string will eventually replace input as the object sent over to index.ejs
+			var str = {
+				type: [],
+				model: [],
+				price: []
+			};
+
+
+			// console.log(str.price[1]);
+
+			for(var i = 1; i < input.length; i++)
+			{
+				// test
+				// console.log(input[i]);
+				var firstTypeIndex = input[i].indexOf('[') + 1;
+				var secondTypeIndex = input[i].indexOf(']');
+				str.type.push(input[i].substring(firstTypeIndex,secondTypeIndex));
+
+				//sometimes users do not add $ and this creates a bad object. Will have to account for this later
+				var firstModelIndex = input[i].indexOf(']') + 2
+				var secondModelIndex = input[i].indexOf('$') - 3;
+				str.model.push(input[i].substring(firstModelIndex,secondModelIndex));
+
+				var firstPriceIndex = input[i].indexOf('$')+1
+				//crap how do I know where the price ends
+				// var secondPriceIndex = input[i].
+
+			}
+
+			console.dir(str.model);
+
+			//send the object
 			res.render('index.ejs',{items: input})
 		}
 
