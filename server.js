1b18270cca1c01bfac9db01c30f871056c3feab8
PashaB <boguslavskiy@gmail.com>
2017-01-29 18:08:13 -0500

    Processing price, added comments on what to do next.

--- server.js
+++ server.js
@@ -61,24 +61,28 @@ app.get('/', (req,res) => {
 				//MINIMIZE EVERYTHING.There's no reason to read the entire string. If you read that part of the string
 				//and processed it, delete it. Make sure to trim again after you substring!
 
-				input[i] = input[i].trim(); //Added and not tested
+				input[i] = input[i].replace(/ /g,''); //Replace all whitespace
+
+
 				var firstTypeIndex = input[i].indexOf('[') + 1;
 				var secondTypeIndex = input[i].indexOf(']');
+
 				str.type.push(input[i].substring(firstTypeIndex,secondTypeIndex));
+				console.dir(input[i].substring(firstTypeIndex,secondTypeIndex))
 
 				//sometimes users do not add $ and this creates a bad object. Will have to account for this later
 
-				var firstModelIndex = input[i].indexOf(']') + 2
-				if(input[i].substring(1,input[i].indexOf('$')).includes('-')) { //Sometimes people skip the [product - $price] format.
-					//YO YOU CAN TRIM() TO GET RID OF WHITESPACES DO THAT LATER
-					var secondModelIndex = input[i].indexOf('$') - 3;
-				} else {
-					var secondModelIndex = input[i].indexOf('$') -1;
-				}
-				
+				var firstModelIndex = input[i].indexOf(']')+1;
+				var secondModelIndex = input[i].indexOf('$')-1;
+
 				str.model.push(input[i].substring(firstModelIndex,secondModelIndex));
+				console.dir(input[i].substring(firstModelIndex,secondModelIndex))
 
 				var firstPriceIndex = input[i].indexOf('$')+1
+				var secondPriceIndex = input[i].indexOf('(');
+
+				str.price.push(input[i].substring(firstPriceIndex,secondPriceIndex));
+				console.dir(input[i].substring(firstPriceIndex,secondPriceIndex));
 				//crap how do I know where the price ends. !! All the titles have (discount calculation) after the price.
 				//Find the first instance of '('. The characters before that are the price. Some titles have no '(' or ')'.
 				// If that's the case just use the rest of the string.
@@ -88,9 +92,15 @@ app.get('/', (req,res) => {
 				//Eventually send model/product name/number to amazon api to reliably find the same product. 
 				//Then update the price with the amazon price. Then we can graph it. 
 
+				// 1/29/17 I think you really need to remove the part of the string processed, and then remove white spaces. Products with
+				//removed whitespaces are hard to read and may be problematic to search.
+
+
+
 			}
 
-			console.dir(str.model);
+			console.dir(str);
+
 
 			//send the object
 			res.render('index.ejs',{items: input})
